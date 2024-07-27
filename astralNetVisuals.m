1e7854555ccd7a5349b05fb3c0c1a681eb57a9f3
Brady Berg <bcberg@gmail.com>
2024-07-27 12:08:59 -0700

    tiny test visuals done

--- astralNetVisuals.m
+++ astralNetVisuals.m
@@ -8,48 +8,79 @@ set(0,'defaultLegendInterpreter','latex')
 
 %% testing generation
 
-astralNum = 2;
-rho = 5;
+% tiny test
+astralNum = 1;
+numAsters = 60;
 l = 5;
-D = 50;
-targetFilNum = rho * D^2 / l;
-numAsters = round(targetFilNum/astralNum,TieBreaker="tozero");  % attempt to match Python rounding behavior;
+D = 10;
 [network,crossings,asters] = generateAstralNetwork(numAsters,l,D,astralNum);
+
+% like cytosim
+% astralNum = 5;
+% rho = 5;
+% l = 5;
+% D = 50;
+% targetFilNum = rho * D^2 / l;
+% numAsters = round(targetFilNum/astralNum,TieBreaker="tozero");  % attempt to match Python rounding behavior;
+% [network,crossings,asters] = generateAstralNetwork(numAsters,l,D,astralNum);
 
 %% visualizing
 
-% % stick plot
-% figure(1)
-% hold on
-% for idx = 1:Nstick
-%     plot(basePoints(idx,1) + l * cos(orient(idx)) * (0:1), ...
-%         basePoints(idx,2) + l * sin(orient(idx)) * (0:1), '-b.')
-% end
-% hold off
-% xlim([30,50])
-% ylim([30,50])
-% saveas(gcf,"C:\Users\bcber\Documents\LaTeX files\figures\astral-mikado_baseSticks.png")
-% 
-% % highlight nodes
-% figure(1)
-% hold on
-% plot(nodes(:,1), nodes(:,2), 'ro')
-% hold off
-% saveas(gcf,"C:\Users\bcber\Documents\LaTeX files\figures\astral-mikado_baseSticks+nodes.png")
-% 
-% % draw spring network
-% figure(2)
-% hold on
-% for idx = 1:size(springs,1)
-%     nodeA = springs(idx,1);
-%     nodeB = springs(idx,2);
-%     coords = [nodes(nodeA,1:2); nodes(nodeB,1:2)];
-%     plot(coords(:,1), coords(:,2), '.b-')
-% end
-% xlim([30,50])
-% ylim([30,50])
-% hold off
-% saveas(gcf,"C:\Users\bcber\Documents\LaTeX files\figures\astral-mikado_springZoom.png")
-% xlim([-0.1*L,1.1*L])
-% ylim([-0.1*L,1.1*L])
-% saveas(gcf,"C:\Users\bcber\Documents\LaTeX files\figures\astral-mikado_springWhole.png")
+% aster plot
+figure(1)
+hold on
+for idx = 1:numAsters
+    if astralNum >= 2
+        plot(asters.centers(idx,1),asters.centers(idx,2),'or')
+    end
+    for jdx = 1:astralNum
+        plot(asters.centers(idx,1) + l * cos(asters.orients(idx,jdx)) * (0:1), ...
+            asters.centers(idx,2) + l * sin(asters.orients(idx,jdx)) * (0:1), ...
+            '-k')
+    end
+end
+hold off
+
+% highlight nodes
+figure(2)
+hold on
+for idx = 1:numAsters
+    if astralNum >= 2
+        plot(asters.centers(idx,1),asters.centers(idx,2),'or')
+    end
+    for jdx = 1:astralNum
+        plot(asters.centers(idx,1) + l * cos(asters.orients(idx,jdx)) * (0:1), ...
+            asters.centers(idx,2) + l * sin(asters.orients(idx,jdx)) * (0:1), ...
+            '-k')
+    end
+end
+plot(network.nodes(:,1),network.nodes(:,2),'*m')
+hold off
+
+% draw spring network
+figure(3)
+hold on
+for idx = 1:size(network.springs,1)
+    augNodeA = network.springs(idx,1);
+    augNodeB = network.springs(idx,2);
+    coords = [network.augNodes(augNodeA,1:2);
+        network.augNodes(augNodeB,1:2)];
+    plot(coords(:,1), coords(:,2),'.k-')
+    if augNodeA <= numAsters && astralNum >= 2
+        % i.e., if augNodeA is an astral center
+        plot(network.augNodes(augNodeA,1),network.augNodes(augNodeA,2),'or')
+    elseif augNodeB <= numAsters && astralNum >= 2
+        % likewise for augNodeB
+        plot(network.augNodes(augNodeB,1),network.augNodes(augNodeB,2),'or')
+    end
+end
+
+% dangling end distribution
+figure(4)
+if astralNum == 1
+    histogram(reshape(network.ends,[2*numAsters,1]))
+elseif astralNum >= 2
+    histogram(network.ends(:,2))
+end
+xlabel('Dangling end length')
+ylabel('Counts')
