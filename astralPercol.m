16d26562fe9e144f2acf6fb0931b2b612a96782d
Brady Berg <bcberg@gmail.com>
2024-10-04 19:38:40 -0700

    curves and heatmaps for all perc defs

--- astralPercol.m
+++ astralPercol.m
@@ -16,12 +16,12 @@ if recompute
         l = 1;
     end
     astralNumList = (1:48)';
-    densSpec = [0,2,50];
+    densSpec = [0,2+log10(5),60];
     Nsamp = 2000;
     Ncores = 8;
 else
     % load a particular existing dataset
-    D = 5;
+    D = 10;
     l = 1;
 end
 % Ubuntu path
@@ -36,61 +36,21 @@ if recompute
     curves = getAllPercCurves(l,D,astralNumList,densSpec,Nsamp,Ncores, ...
         saveDirectory);
 end
-
-%% Plotting
-
 load(fullfile(saveDirectory,filename + ".mat"))
-
-% fig1 = figure(1);
-% set(fig1,'defaultLineLineWidth',0.75)
-% cmap = colormap(turbo(numNetTypes));
-% hold on
-% for idx = 1:numNetTypes
-%     plot(actualDensities{idx},percProbs{idx},'DisplayName', ...
-%         sprintf('nAST%02i',astralNumList(idx)),'Color',cmap(idx,:))
-% end
-% hold off
-% xscale('log')    % 'linear' or 'log'
-% legend('Location','eastoutside','NumColumns',2)
-% xlabel('Filament density [$\mu m^{-1}$]')
-% ylabel('Percolation probability')
-% title(sprintf('Domain size: %2.1f, Filament length: %1.1f',D,l))
-% exportgraphics(fig1,fullfile(saveDirectory,filename + "_curves.png"), ...
-%     'Resolution',300)
-% 
-% %% Heatmap
-% 
-% % For heatmap, need to interpolate values at each value of densityRange
-% % Currently using cubic splines
-% numDensVals = length(densityRange);
-% percProbsHeatmap = zeros(numNetTypes,numDensVals);
-% for idx = 1:numNetTypes
-%     percProbsHeatmap(idx,:) = spline(actualDensities{idx}, ...
-%         percProbs{idx},densityRange);
-% end
-% 
-% fig2 = figure(2);
-% pcolor(densityRange,astralNumList,percProbsHeatmap)
-% xscale('log')
-% xlabel('Filament density [$\mu m^{-1}$]')
-% ylabel('Astral number')
-% title(sprintf('Domain size: %2.1f, Filament length: %1.1f',D,l))
-% exportgraphics(fig2,fullfile(saveDirectory,filename + "_heat.png"), ...
-%     'Resolution',300)
 
 %% Curves for all percolation definitions
 
-thisFig = figure(99);
-set(thisFig,'Position',[200,200,300*8.5,300*11],'visible','on', ...
-    'defaultLineLineWidth',0.75)
-thisFig = tiledlayout(3,2,'TileSpacing','compact');
+fig1 = figure(1);
+set(fig1,'units','inches','Position',[1,1,8.5,11],'visible','off', ...
+    'defaultLineLineWidth',0.6)
+fig1 = tiledlayout(3,2,'TileSpacing','compact');
 
 percTypes = {"Top-to-Bottom (TB)", "Left-to-Right (LR)", "TB OR LR", ...
     "TB AND LR", "Single connected component"};
 numPercTypes = length(percTypes); % i.e., = 5
 cmap = colormap(turbo(numNetTypes));
 for pdx = 1:numPercTypes
-    nexttile(thisFig,mod(pdx,6) + 6 * (mod(pdx,6)==0))
+    nexttile(fig1,mod(pdx,6) + 6 * (mod(pdx,6)==0))
     hold on
     for idx = 1:numNetTypes
         plot(actualDensities{idx},percProbs{idx}(pdx,:),'DisplayName', ...
@@ -102,49 +62,46 @@ for pdx = 1:numPercTypes
     ylabel('Percolation probability')
     title(percTypes{pdx})
     if pdx == 1
-        lg = legend('Location','NumColumns',2);
+        lg = legend('NumColumns',4,'FontSize',12);
         lg.Layout.Tile = 6;
     end
 end
-title(thisFig,sprintf('Domain size: %2.1f, Filament length: %1.1f',D,l))
-% for idx = 1:length(nFilPerAsterList)
-%     nexttile(thisFig,mod(idx,6) + 6 * (mod(idx,6)==0))
-%     nFilPerAster = nFilPerAsterList(idx);
-%     networkLabel = sprintf('n_{AST}=%i',nFilPerAster);
-% 
-%     h = histogram(displacements(idx,:),'Normalization','probability');
-%     h.BinWidth = 0.1;
-%     xlim([0,5])
-%     ylim([0,0.5])
-%     xlabel('x-Displacement [{\mu}m]')
-%     ylabel('Empirical probability')
-%     title([networkLabel,sprintf(', mean = %.3f',meandisps(idx))])
-% 
-%     if 0 == mod(idx,6) && idx == 6
-%         % export first page
-%         % Ubuntu path
-%         exportgraphics(thisFig,'~/Documents/AstralMikadoCYM/data/displace_distr_shear_50x50.pdf', ...
-%             'Resolution',300)
-%         % Windows path
-%         % exportgraphics(thisFig,'C:\Users\bcber\Documents\AstralMikadoCYM\data\displace_distr_shear_50x50.pdf', ...
-%         %     'Resolution',300)
-%         thisFig = clf(thisFig);
-%         set(thisFig,'Position',[200,200,300*8.5,300*11],'visible','off', ...
-%             'defaultLineLineWidth',0.75)
-%         thisFig = tiledlayout(3,2,'TileSpacing','compact');
-%     elseif 0 == mod(idx,6) && idx > 6
-%         % export next pages, append to first file
-%         % Ubuntu path
-%         exportgraphics(thisFig,'~/Documents/AstralMikadoCYM/data/displace_distr_shear_50x50.pdf', ...
-%             'Resolution',300,'Append',true)
-%         % Windows path
-%         % exportgraphics(thisFig,'C:\Users\bcber\Documents\AstralMikadoCYM\data\displace_distr_shear_50x50.pdf', ...
-%         %     'Resolution',300,'Append',true)
-%         thisFig = clf(thisFig);
-%         set(thisFig,'Position',[200,200,300*8.5,300*11],'visible','off', ...
-%             'defaultLineLineWidth',0.75)
-%         thisFig = tiledlayout(3,2,'TileSpacing','compact');
-%     else
-%         legend('off')
-%     end
-% end
+title(fig1,sprintf('Domain size: %2.1f, Filament length: %1.1f',D,l))
+exportgraphics(fig1,fullfile(saveDirectory,filename + "_curves.pdf"), ...
+    'Resolution',300)
+
+%% Heatmaps for all percolation definitions
+
+fig2 = figure(2);
+set(fig2,'units','inches','Position',[1,1,8.5,11],'visible','off', ...
+    'defaultLineLineWidth',0.6)
+fig2 = tiledlayout(3,2,'TileSpacing','compact');
+
+percTypes = {"Top-to-Bottom (TB)", "Left-to-Right (LR)", "TB OR LR", ...
+    "TB AND LR", "Single connected component"};
+numPercTypes = length(percTypes); % i.e., = 5
+cmap = colormap(parula(numNetTypes));
+for pdx = 1:numPercTypes
+    nexttile(fig2,mod(pdx,6) + 6 * (mod(pdx,6)==0))
+    % For heatmap, need to interpolate values at each value of densityRange
+    % Currently using cubic splines
+    numDensVals = length(densityRange);
+    heatmap = zeros(numNetTypes,numDensVals);
+    for idx = 1:numNetTypes
+        heatmap(idx,:) = clip(spline(log10(actualDensities{idx}), ...
+            percProbs{idx}(pdx,:),log10(densityRange)),0,1);
+    end
+
+    pcolor(densityRange,astralNumList,heatmap)
+    xscale('log')
+    xlabel('Filament density [$\mu m^{-1}$]')
+    ylabel('Astral number')
+    title(percTypes{pdx})
+    if pdx == 1
+        bar = colorbar;
+        bar.Layout.Tile = 6;
+    end
+end
+title(fig2,sprintf('Domain size: %2.1f, Filament length: %1.1f',D,l))
+exportgraphics(fig2,fullfile(saveDirectory,filename + "_heat.pdf"), ...
+    'Resolution',300)
